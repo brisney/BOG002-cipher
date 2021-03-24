e7ebcfc1727bc1e1c3e03f8334b08d7225125f37
brisney quiñonez <brisneyducuara@gmail.com>
2021-03-24 13:38:24 -0500

    se muestra en la consola el dato ingresado por el usuario

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+let inputcifrar = document.getElementById("cifrar");
+let inputespacioclave = document.getElementById("espacioclave");
+let inputespacioposiciones = document.getElementById("espacioposiciones");
+
+// inputcifrar.onclick = function mostrarvalores () {
+//     let clavecifrar = inputespacioclave.value;  
+//     // cambiarHTML(clavecifrar);
+//     console.log (clavecifrar)
+// }
+
+inputcifrar.addEventListener("click",mostrarvalores); 
+function mostrarvalores () {
+    let clavecifrar = inputespacioclave.value; 
+    let posiciones = inputespacioposiciones.value
+    console.log (clavecifrar,posiciones);
+} 
